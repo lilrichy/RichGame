2a23d768e2d648a8fa9893716d50b5d462cec110
lilrichy <richardreigens@gmail.com>
2014-08-08 14:27:57 -0400

    Initial commit - with fadeing splash screen

---
+++ core/src/com/reigens/MasterWarrior.java
@@ -0,0 +1,53 @@
+package com.reigens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.reigens.screens.Splash;
+
+public class MasterWarrior extends Game
+{
+    public static final String TITLE = "Master Warrior";
+    public static final String VERSION = "0.0.0.2";
+    SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+	setScreen(new Splash());
+	}
+
+    @Override
+    public void dispose()
+    {
+    super.dispose();
+    }
+
+	@Override
+	public void render () {
+	super.render();
+	}
+
+
+    @Override
+    public void resize(int width, int height)
+    {
+super.resize(width, height);
+    }
+
+
+
+    @Override
+    public void pause()
+    {
+super.pause();
+    }
+
+    @Override
+    public void resume()
+    {
+super.resume();
+    }
+
+
+}
